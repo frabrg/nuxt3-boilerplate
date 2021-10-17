b4062116b570767668d9dcf2d95b15caf41db37c
Fraberg <frcs.berger@gmail.com>
2021-10-17 22:59:29 +0200

    fix(icons): Remvoved import generating build error

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,15 @@
 	<div class="container">
 		<div class="flex items-center">
 			<p class="text-green-200">{{ indexContent }}</p>
-			<CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
+			<!-- https://github.com/tailwindlabs/heroicons/issues/528 -->
+			<!-- <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" /> -->
 		</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
-import { CheckIcon } from "@heroicons/vue/outline";
+// https://github.com/tailwindlabs/heroicons/issues/528
+// import { CheckIcon } from "@heroicons/vue/outline";
 
 const indexContent = "eslint/prettier/stylelint now works";
 </script>
