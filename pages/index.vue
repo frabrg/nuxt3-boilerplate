05167b0ba3dd5c25d1c8910cd531d29cabf1cc34
Fraberg <frcs.berger@gmail.com>
2021-10-17 15:25:42 +0200

    feature(icons): Added heroicons

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
 	<div class="container">
-		<p class="text-green-200">{{ indexContent }}</p>
+		<div class="flex items-center">
+			<p class="text-green-200">{{ indexContent }}</p>
+			<CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
+		</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
+import { CheckIcon } from "@heroicons/vue/outline";
+
 const indexContent = "eslint/prettier/stylelint now works";
 </script>
